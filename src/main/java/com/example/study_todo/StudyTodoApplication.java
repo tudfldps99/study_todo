8b0ecce1d55765a926c46499d5b72bbd3af472dc
tudfldps99 <tudfldps99@naver.com>
2023-02-04 21:11:19 +0900

    # 초기 설정

---
+++ src/main/java/com/example/study_todo/StudyTodoApplication.java
@@ -0,0 +1,13 @@
+package com.example.study_todo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyTodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyTodoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/study_todo/StudyTodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.study_todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyTodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
