373a0ac6c8350d3a25587a2e6990e0dde020db46
tudfldps99 <tudfldps99@naver.com>
2023-02-05 14:44:23 +0900

    # Controller 작성 (TodoApiController)

--- README.md
+++ README.md
@@ -51,3 +51,37 @@
 7. src/main/java/com/example/study_todo/dto/request/TodoCreateRequestDTO.java 생성 및 작성
 8. src/main/java/com/example/study_todo/dto/request/TodoModifyRequestDTO.java 생성 및 작성
 9. Test 파일 생성 = repository 에서 Ctrl + Shift + t 로 테스트파일 생성 (TodoServiceTest.java)
+
+- Controller
+: 사용자의 요청을 처리한 뒤 지정된 뷰에 모델 객체를 넘겨주는 역할. 
+  즉, 사용자의 요청이 진입하는 지점, 요청에 따라 어떤 처리를 할지 결정을 Service 에게 넘겨줌. 그 후 Service 에서 실질적으로 처리한 내용을 view 에게 넘겨줌 
+
+10. src/main/java/com/example/study_todo/controller/TodoApiController.java 생성 및 작성
+11. PostMan 에서 API 요청하며 Test
+
+
+---
+## 필기
+- Health Check : 서버 상태 확인
+- internalServerError() : Internal Server Error 가 나오는 경우는 에러 코드로 500번대 에러 메세지가 나오는 경우 (서버의 문제)
+- HttpServletRequest : PUT 인지, PATCH 인지 요청정보를 알 수있음
+
+### annotation
+- @RestController: JSON 으로 데이터를 주고받음을 선언 
+- @Slf4j: 로깅 추상화 라이브러리
+- @Getter, @Setter : 접근자/설정자 자동 생성 
+- @ToString : toString() 메소드 자동 생성 
+- @NoArgsConstructor : 생성자 자동 생성 - 파라미터가 없는 기본 생성자 
+- @AllArgsConstructor : 생성자 자동 생성 - 모든 필드 값을 파라미터로 받는 생성자 
+- @RequiredArgsConstructor : 초기화 되지 않은 final 필드나, @NonNull 이 붙은 필드에 대해 생성자를 생성해줌
+- @EqualsAndHashCode(of = "todoId") : equals, hashCode 메소드 자동 생성 
+  (equals : 두 객체의 내용이 같은지, 동등성을 비교하는 연산자 / hashCode : 두 객체가 같은 객체인지, 동일성을 비교하는 연산자)
+- @Builder : 빌드 패턴 자동 생성
+- @Entity : 데이터베이스 테이블
+- @Table(name = "tbl_todo")       // 테이블 명칭 지정 (tbl_todo)
+- @Id : 테이블 상의 Primary Key 
+- @GeneratedValue(generator = "system-uuid") : 랜덤으로 생성
+- @Validated : 데이터 유효성 검증
+- @RequestBody : HttpRequest 의 본문 RequestBody 의 내용을 자바 객체로 매핑 
+- @BindingResult : ModelAttribute 을 이용하여 매개변수를 Bean 에 binding 할 때, 발생한 오류 정보를 받기 위해 선언해야 하는 어노테이션
+- @PathVariable("id") : id를 경로에서 읽음
---
+++ src/main/java/com/example/study_todo/controller/TodoApiController.java
@@ -0,0 +1,132 @@
+package com.example.study_todo.controller;
+
+import com.example.study_todo.dto.request.TodoCreateRequestDTO;
+import com.example.study_todo.dto.request.TodoModifyRequestDTO;
+import com.example.study_todo.dto.response.TodoListResponseDTO;
+import com.example.study_todo.service.TodoService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+
+@RestController     // controller + @ResponseBody --> JSON/XML 형태로 객체 데이터 반환 목적
+@Slf4j
+@RequiredArgsConstructor
+@RequestMapping("/api/todos")
+public class TodoApiController {
+
+    private final TodoService todoService;
+
+    // 할 일 목록 요청 (GET)
+    @GetMapping
+    public ResponseEntity<?> listTodo() {
+        log.info("/api/todos GET request");
+
+        TodoListResponseDTO responseDTO = todoService.retrieve();
+
+        return ResponseEntity
+                .ok()
+                .body(responseDTO);
+    }
+
+    // 할 일 등록 요청 (POST)
+    @PostMapping
+    public ResponseEntity<?> createTodo(@Validated @RequestBody TodoCreateRequestDTO requestDTO, BindingResult result) {        // Validated : 데이터 유효성 검증
+                                                                                                                                // RequestBody : HttpRequest 의 본문 RequestBody 의 내용을 자바 객체로 매핑
+                                                                                                                                // BindingResult : ModelAttribute 을 이용하여 매개변수를 Bean 에 binding 할 때, 발생한 오류 정보를 받기 위해 선언해야 하는 어노테이션
+        log.info("/api/todos POST request");
+
+        if (result.hasErrors()) {       // 검증 에러가 발생하면 badRequest
+            log.warn("DTO 검증 에러 발생: {}", result.getFieldError());
+
+            return ResponseEntity
+                    .badRequest()
+                    .body(result.getFieldError());
+        }
+
+        try {
+            TodoListResponseDTO responseDTO = todoService.create(requestDTO);
+
+            return ResponseEntity
+                    .ok()
+                    .body(responseDTO);
+        } catch (RuntimeException e) {
+            log.error(e.getMessage());
+
+            return ResponseEntity
+                    .internalServerError()          // Internal Server Error 가 나오는 경우는 에러 코드로 500번대 에러 메세지가 나오는 경우 (서버의 문제)
+                    .body(TodoListResponseDTO
+                            .builder()
+                            .error(e.getMessage()));        // TodoListResponseDTO.java 의 error 에 담김
+        }
+    }
+
+    // 할 일 수정 요청 (PUT, PATCH)
+    @RequestMapping(value = "/{id}", method = {RequestMethod.PUT, RequestMethod.PATCH})
+    public ResponseEntity<?> updateTodo(
+                @PathVariable("id") String todoId,
+                @Validated @RequestBody TodoModifyRequestDTO requestDTO,
+                BindingResult result,
+                HttpServletRequest request      // PUT 인지, PATCH 인지 요청정보를 알 수있음
+    ) {
+        if (result.hasErrors()) {
+            return ResponseEntity
+                    .badRequest()
+                    .body(result.getFieldError());
+        }
+
+        log.info("/api/todos/{} {} request", todoId, request.getMethod());
+        log.info("modifying dto: {}", requestDTO);
+
+        try {
+            TodoListResponseDTO responseDTO = todoService.modify(todoId, requestDTO);
+
+            return ResponseEntity
+                    .ok()
+                    .body(responseDTO);
+        } catch (Exception e) {
+            return ResponseEntity
+                    .internalServerError()
+                    .body(TodoListResponseDTO
+                            .builder()
+                            .error(e.getMessage()));
+        }
+    }
+
+    // 할 일 삭제 요청 (DELETE)
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteTodo(@PathVariable("id") String todoId) {    // PathVariable : id를 경로에서 읽음
+
+        log.info("/api/todos/{} DELETE request", todoId);
+
+        if (todoId == null || todoId.equals("")) {
+            return ResponseEntity
+                    .badRequest()
+                    .body(TodoListResponseDTO
+                            .builder()
+                            .error("ID를 전달해주세요"));      // TodoListResponseDTO.java 의 error 에 담김
+        }
+
+        try {
+            TodoListResponseDTO responseDTO = todoService.delete(todoId);
+
+            return ResponseEntity
+                    .ok()
+                    .body(responseDTO);
+        } catch(Exception e) {
+            log.error(e.getMessage());
+
+            return ResponseEntity
+                    .internalServerError()
+                    .body(TodoListResponseDTO
+                            .builder()
+                            .error(e.getMessage()));        // e.getMessage() 는 TodoService.java 에서 delete 메소드의 throw new RuntimeException() 을 의미
+        }
+
+    }
+
+}
